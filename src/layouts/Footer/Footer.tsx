bf8bfc8c361c06394dbebb6c2fc812b7055df7e3
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-05 14:51:12 +0300

    Navbar ve footer tasarımı yapıldı

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  MenuMenu,
+  Image,
+  Container,
+} from "semantic-ui-react";
 
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="fixed-bottom " style={{ backgroundColor: "#9933ff" }}>
+      <Container borderless>
+        <Menu
+          className="menu"
+          size="large"
+          style={{ backgroundColor: "#9933ff" }}
+          borderless
+          fixed="bottom"
+        >
+          <Container>
+            <MenuItem className="menuitem">
+              <Image
+                src="Images/Tebeto-logo-yatay-beyaz.png"
+                size="small"
+                align="left"
+              />
+            </MenuItem>
 
-export default Footer
+            <MenuMenu position="right">
+              <MenuItem>
+                <Button>Bize ulaşın</Button>
+              </MenuItem>
+            </MenuMenu>
+          </Container>
+        </Menu>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import NavButton from './NavButton'
+import React from "react";
+import { MenuItem, Menu, MenuMenu, Button, Image, Container } from "semantic-ui-react";
 
-type Props = {}
+const Navbar = () => {
+  const containerStyle = {
+    textAlign: 'center',
+    flexDirection: 'row',
+    height: '100%',
+    justifyContent: 'center',
+  };
 
-const Navbar = (props: Props) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Menu className="menu" size="large" borderless>
+      <MenuItem className="menuitem">
+        <Image src="Images/tobeto-logo.png" size="small" />
+      </MenuItem>
 
+      <Container style={containerStyle}>
+        <MenuItem name="Anasayfa" style={{ fontWeight: "bold" }} />
+        <MenuItem name="Profilim" style={{ fontWeight: "bold" }} />
+        <MenuItem name="Değerlendirmeler" style={{ fontWeight: "bold" }} />
+        <MenuItem name="Katalog" style={{ fontWeight: "bold" }} />
+        <MenuItem name="Takvim" style={{ fontWeight: "bold" }} />
+        <MenuItem name="İstanbul Kodluyor" style={{ fontWeight: "bold" }} />
+      </Container>
 
+      <MenuMenu>
+        <MenuItem>
+          <Button primary>Giriş Yap</Button>
+        </MenuItem>
+      </MenuMenu>
+    </Menu>
+  );
+};
 
-export default Navbar
+export default Navbar;
