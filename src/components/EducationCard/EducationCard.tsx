b730603352cd6c6671bd9e6d6fc536192238aaee
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-08 09:15:35 +0300

    Education kart düzenlendi

--- src/components/EducationCard/EducationCard.tsx
+++ src/components/EducationCard/EducationCard.tsx
@@ -6,14 +6,14 @@ type Props = {}
 const EducationCard = (props: Props) => {
   return (
     <div>
-  <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://tobeto.s3.cloud.ngn.com.tr/23_EAH_1_45f7232003.jpg" />
+  <Card className='card' style={{ width: '16rem' }}>
+      <Card.Img className='card-img' src="https://tobeto.s3.cloud.ngn.com.tr/23_EAH_1_45f7232003.jpg" />
       <Card.Body>
         <Card.Title>Dr. Ecmel Ayral'dan Hoşgeldin Mesajı</Card.Title>
-        <Card.Text>
+        <Card.Text className='platform-course-date'>
         21 Eylül 2023 15:20
         </Card.Text>
-        <Button variant="primary">Eğitime Git</Button>
+        <Button className='apply-button'>Eğitime Git</Button>
       </Card.Body>
     </Card></div>
   )
