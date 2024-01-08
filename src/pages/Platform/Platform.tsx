d538ada681c1a5564ac3342586b71f54ecc066dc
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-08 08:27:20 +0300

    platform güncellendi

--- src/pages/Platform/Platform.tsx
+++ src/pages/Platform/Platform.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 import TobetoPlatformItem from "../../utilities/tobetoPlatform/TobetoPlatformItem";
 import imageSize from "../../enums/imageSize";
+import { Container } from "semantic-ui-react";
+import PlatformTab from "../../components/PlatformTab/PlatformTab";
 type Props = {};
-
-const style = {
-  fontSize: "16px",
-  fontWeight: 700,
-  color: "rgba(40, 40, 40, 0.8)",
-  margin: "auto 0",
-  border: "none",
-};
 
 const Platform = (props: Props) => {
   return (
     <>
-    <div style={{display:"flex", flexDirection:"column", gap:"9rem", marginTop:"9rem"}}>
-      <TobetoPlatformItem
-        headerText1="TOBETO"
-        headerText2="'ya hoş geldin"
-        description="Nil"
-        subDescription="Yeni nesil öğrenme deneyimi ile Tobeto kariyer yolculuğunda senin yanında!"
-      />
-      <TobetoPlatformItem
-        imageSrc="istanbulKodluyor.png" 
-        imageSize={imageSize.medium}
-        description="Ücretsiz eğitimlerle, geleceğin mesleklerinde sen de yerini al."
-        subDescription="Aradığın  “İş”  Burada!"
-      />
-    </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "9rem",
+          marginTop: "9rem",
+        }}
+      >
+        <TobetoPlatformItem
+          headerText1="TOBETO"
+          headerText2="'ya hoş geldin"
+          description="Nil"
+          subDescription="Yeni nesil öğrenme deneyimi ile Tobeto kariyer yolculuğunda senin yanında!"
+        />
+        <Container className="content-cont">
+          <TobetoPlatformItem
+            imageSrc="istanbulKodluyor.png"
+            imageSize={imageSize.medium}
+            description="Ücretsiz eğitimlerle, geleceğin mesleklerinde sen de yerini al."
+            subDescription={["Aradığınız ", <span style={{color: "#00d29b"}}>“</span>, "İŞ" ,<span style={{color: "#00d29b"}}>“</span>, " burada!"]}
+          />
+          <PlatformTab />
+        </Container>
+      </div>
     </>
   );
 };
 
 export default Platform;
-
