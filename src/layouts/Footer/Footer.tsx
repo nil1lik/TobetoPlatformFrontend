157e712bf6de23d3ab78b58b941de6b29ced40fa
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-08 08:08:34 +0300

    platform anasayfa düzenlemesi

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -4,12 +4,19 @@ import { Button, Container, Image, Menu } from "semantic-ui-react";
 type Props = {};
 const logoSrc = process.env.PUBLIC_URL + "/images/Tebeto-logo-yatay-beyaz.png";
 
+const divStyle = {
+  height: '100px', 
+  display: 'flex', 
+  alignItems: 'center', 
+  justifyContent: 'center',
+};
+
 const Footer = (props: Props) => {
   return (
-    <Menu pointing secondary size="huge" style={{ backgroundColor: "#9b33ff" }}>
+    <Menu pointing secondary size="huge" fixed="bottom" style={{ ...divStyle, backgroundColor: "#9b33ff" }}>
       <Container>
         <Menu.Item>
-          <Image src={logoSrc} size="small" />
+          <Image src={logoSrc} size="tiny" />
         </Menu.Item>
         <Menu.Item position="right">
           <Button style={{ backgroundColor:"#ffffff", color:"#000000", borderRadius:"20px", fontSize:"14px", fontWeight:"800"}}>Bize Ulaşın</Button>
@@ -20,3 +27,4 @@ const Footer = (props: Props) => {
 };
 
 export default Footer;
+
--- src/pages/Platform/Platform.tsx
+++ src/pages/Platform/Platform.tsx
@@ -1,27 +1,54 @@
 import React from "react";
 import TobetoPlatformItem from "../../utilities/tobetoPlatform/TobetoPlatformItem";
 import imageSize from "../../enums/imageSize";
+import { Menu, MenuItem, Segment } from "semantic-ui-react";
+import Application from "../../components/Application/Application";
+
 type Props = {};
+
+const style = {
+  fontSize: "16px",
+  fontWeight: 700,
+  color: "rgba(40, 40, 40, 0.8)",
+  margin: "auto 0",
+  border: "none",
+};
 
 const Platform = (props: Props) => {
   return (
     <>
-    <div style={{display:"flex", flexDirection:"column", gap:"9rem", marginTop:"9rem"}}>
-      <TobetoPlatformItem
-        headerText1="TOBETO"
-        headerText2="'ya hoş geldin"
-        description="Nil"
-        subDescription="Yeni nesil öğrenme deneyimi ile Tobeto kariyer yolculuğunda senin yanında!"
-      />
-      <TobetoPlatformItem
-        imageSrc="istanbulKodluyor.png" 
-        imageSize={imageSize.medium}
-        description="Ücretsiz eğitimlerle, geleceğin mesleklerinde sen de yerini al."
-        subDescription="Aradığın  “İş”  Burada!"
-      />
-    </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "9rem",
+          marginTop: "9rem",
+        }}
+      >
+        <TobetoPlatformItem
+          headerText1="TOBETO"
+          headerText2="'ya hoş geldin"
+          description="Nil"
+          subDescription="Yeni nesil öğrenme deneyimi ile Tobeto kariyer yolculuğunda senin yanında!"
+        />
+        <Segment>
+          <TobetoPlatformItem
+            imageSrc="istanbulKodluyor.png"
+            imageSize={imageSize.medium}
+            description="Ücretsiz eğitimlerle, geleceğin mesleklerinde sen de yerini al."
+            subDescription="Aradığın “İş” Burada!"
+          />
+          <Menu pointing secondary>
+            <MenuItem style={style}  name="Başvurularım" Component={Application}  />
+            <MenuItem style={style}  name="Eğitimlerim" />
+            <MenuItem style={style} name="Duyuru ve Haberlerim" />
+            <MenuItem style={style} name="Anketlerim" />
+          </Menu>
+        </Segment>
+      </div>
     </>
   );
 };
 
 export default Platform;
+
