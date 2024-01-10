13a6d81d9051f8ef8867bba48564a9fe0ac24b33
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-10 09:25:46 +0300

    Navigation alındı

--- src/layouts/Navbar/Navigation.tsx
+++ src/layouts/Navbar/Navigation.tsx
@@ -1,32 +1,43 @@
-import { Image, NavDropdown, Navbar, Row } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
+import { Image, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 type Props = {};
 const logo = process.env.PUBLIC_URL + "/images/tobeto-logo.png"
 const Navigation = (props: Props) => {
   return (
     <Navbar expand="lg" className=" justify-content-between custom-navbar" bg="light" data-bs-theme="light">
-        <Image src="./images/tobeto-logo.png" width={170} height={35}></Image>
-          <Nav className="nav-style">
-            <Nav.Link href="#anasayfa">Anasayfa</Nav.Link>
-            <Nav.Link href="#profilim">Profilim</Nav.Link>
-            <Nav.Link href="#degerlendirmeler">Değerlendirmeler</Nav.Link>
-            <Nav.Link href="#katalog">Katalog</Nav.Link>
-            <Nav.Link href="#takvim">Takvim</Nav.Link>
-            <Nav.Link href="#istanbul-kodluyor">İstanbul Kodluyor</Nav.Link>
-          </Nav>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+      <Image src={logo} width={170} height={35}></Image>
+      <Nav className="nav-style">
+        <Link to={"/"}>
+          <Nav.Link href="#anasayfa">Anasayfa</Nav.Link>
+        </Link>
+        <Link to={"/profilim"}>
+          <Nav.Link href="#profilim">Profilim</Nav.Link>
+        </Link>
+        <Link to={"/degerlendirmeler"}>
+          <Nav.Link href="#degerlendirmeler">Değerlendirmeler</Nav.Link>
+        </Link>
+        <Link to={"/katalog"}>
+          <Nav.Link href="#katalog">Katalog</Nav.Link>
+        </Link>
+        <Link to={"/takvim"}>
+          <Nav.Link href="#takvim">Takvim</Nav.Link>
+        </Link>
+        <Link to={"/istanbulkodluyor"}>
+          <Nav.Link href="#istanbul-kodluyor">İstanbul Kodluyor</Nav.Link>
+        </Link>
+      </Nav>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">
+          Another action
+        </NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">
+          Separated link
+        </NavDropdown.Item>
+      </NavDropdown>
     </Navbar>
   );
 };
