2688366f8e596e9d23bb06eeee97b8a49646247c
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-10 08:17:38 +0300

    istanbul kodluyor linki verildi

--- src/layouts/Navbar/Navigation.tsx
+++ src/layouts/Navbar/Navigation.tsx
@@ -1,6 +1,7 @@
 import { Image, NavDropdown, Navbar, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
@@ -9,12 +10,12 @@ const Navigation = (props: Props) => {
     <Navbar expand="lg" className=" justify-content-between custom-navbar" bg="light" data-bs-theme="light">
         <Image src="./images/tobeto-logo.png" width={170} height={35}></Image>
           <Nav className="nav-style">
-            <Nav.Link href="#anasayfa">Anasayfa</Nav.Link>
+            <Link to={"/"}><Nav.Link href="#anasayfa">Anasayfa</Nav.Link></Link>
             <Nav.Link href="#profilim">Profilim</Nav.Link>
             <Nav.Link href="#degerlendirmeler">Değerlendirmeler</Nav.Link>
             <Nav.Link href="#katalog">Katalog</Nav.Link>
             <Nav.Link href="#takvim">Takvim</Nav.Link>
-            <Nav.Link href="#istanbul-kodluyor">İstanbul Kodluyor</Nav.Link>
+            <Nav.Link href="https://tobeto.com/istanbul-kodluyor">İstanbul Kodluyor</Nav.Link>
           </Nav>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
