8aaa777231df9b70abce530433457614239d63a8
Eren Macit <ernmctt@gmail.com>
2024-01-11 23:54:13 +0300

    feat: Added ProfileInformationPage

---
+++ src/components/ProfileEdit/ProfileInput.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Field } from 'formik'
+import FormikInput from '../../utilities/FormikInput/FormikInput'
+
+type Props = {
+    className?: string,
+    as?: string,
+    type: string,
+    name: string,
+    label: string,
+    placeholder?: string
+}
+
+const ProfileInput = (props: Props) => {
+    return (
+        // <Field
+        //     as={props.as}
+        //     type={props.type}
+        //     name={props.name}
+        //     label={props.label}
+        //     placeHolder={props.placeholder}
+        // />
+        <FormikInput
+        type={props.type}
+        name={props.name}
+        label={props.label}
+        placeHolder={props.placeholder}
+    />
+    )
+}
+
+export default ProfileInput
