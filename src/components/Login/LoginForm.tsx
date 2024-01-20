ab609c8f35d5f02c90b648cd850d8be7362b2b4b
Nil <144815514+nil1lik@users.noreply.github.com>
2024-01-20 15:56:01 +0300

    LoginForm edited. Bugs fixed.

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,48 +1,49 @@
-import { Field, Formik } from 'formik'
-import React from 'react'
-import { Button, Form, Image, Row } from 'react-bootstrap'
-import FormikInput from '../../utilities/FormikInput'
+import { Field, Formik } from "formik";
+import React from "react";
+import { Button, Form, Image, Row } from "react-bootstrap";
+import FormikInput from "../../utilities/FormikInput";
 
-type Props = {image: string}
+type Props = { image: string };
 
 const LoginForm = (props: Props) => {
-    const initialValues={}
-    const formLogo = process.env.PUBLIC_URL + `/images/${props.image}`;
+  const initialValues = {};
+  const formLogo = process.env.PUBLIC_URL + `/images/${props.image}`;
   return (
-        
-        <Formik 
-        initialValues={initialValues}
-          onSubmit={(values) => {
-          console.log(values);
-          }}
-        >
-            <Form className='loginform-cont'>
-                <Row>
-                    <Image className="login-form-img" src={formLogo}/>
-                </Row>
-                <Row>
-                    <FormikInput 
-                        type='text'
-                        name='e-posta'
-                        label=''
-                        placeHolder='E-posta'/>
-                    <FormikInput
-                        type='password'
-                        name='password'
-                        label=''
-                        placeHolder='Şifre'/>
-                </Row>
-                <Row>
-                <button
-              type="submit"
-              className="button-save py-2 mb-3 mt-4 d-inline-block form-btn"
-            >Giriş Yap</button>
-                </Row>
-            </Form>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      <Form className="loginform-cont">
+        <Row>
+          <Image className="login-form-img" src={formLogo} />
+        </Row>
+        <Row>
+          <FormikInput
+            type="text"
+            name="e-posta"
+            label=""
+            placeHolder="E-posta"
+          />
+          <FormikInput
+            type="password"
+            name="password"
+            label=""
+            placeHolder="Şifre"
+          />
+        </Row>
+        <Row className="row-btn">
+          <button
+            type="submit"
+            className="button-save py-2 mb-3 mt-4 d-inline-block"
+          >
+            Giriş Yap
+          </button>
+        </Row>
+      </Form>
+    </Formik>
+  );
+};
 
-        </Formik>
-    
-  )
-}
-
-export default LoginForm
+export default LoginForm;
