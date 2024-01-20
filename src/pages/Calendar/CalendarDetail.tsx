31efafd1f373aadc6caec7000677cce40c7cae91
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-20 15:11:05 +0300

    Takvim sayfasına Dropdown eklendi

--- src/pages/Calendar/CalendarDetail.tsx
+++ src/pages/Calendar/CalendarDetail.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Calendar from "./Calendar";
-import { Col, Form, Row } from "react-bootstrap";
+import { Col, Dropdown, Form, Row } from "react-bootstrap";
 import "./calendar.css";
 import { FormCheckType } from "react-bootstrap/esm/FormCheck";
+import { Container } from "react-bootstrap/lib/Tab";
 
 type Props = {};
 const CalendarDetail = (props: Props) => {
@@ -27,39 +28,52 @@ const CalendarDetail = (props: Props) => {
             </div>
             <div className="d-flex flex-column">
               <label className="label-header">Eğitmen</label>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Eğitmen arayın...
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Engin Demirog</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Halit Enes Kalaycı
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">İrem hoca</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
             <div className="d-flex flex-column">
               <label className="label-header">Eğitim Durumu</label>
               <div className="check-class">
-              <Form>
-                {["checkbox"].map((type) => (
-                  <div key={`inline-${type}`} className="mb-3">
-                    <Form.Check
-                      inline
-                      label="Bitmiş dersler"
-                      id={`inline-${type}-1`}
-                    />
-                    <br/>
-                    <Form.Check
-                      inline
-                      label="Devam eden dersler"
-                      id={`inline-${type}-1`}
-                    />
-                    <br/>
-                    <Form.Check
-                      inline
-                      label="Satın alınmış dersler"
-                      id={`inline-${type}-1`}
-                    />
-                    <br/>
-                    <Form.Check
-                      inline
-                      label="Başlanmamış dersler"
-                      id={`inline-${type}-1`}
-                    />
-                  </div>
-                ))}
-              </Form>
+                <Form>
+                  {["checkbox"].map((type) => (
+                    <div key={`inline-${type}`} className="mb-3">
+                      <Form.Check
+                        inline
+                        label="Bitmiş dersler"
+                        id={`inline-${type}-1`}
+                      />
+                      <br />
+                      <Form.Check
+                        inline
+                        label="Devam eden dersler"
+                        id={`inline-${type}-1`}
+                      />
+                      <br />
+                      <Form.Check
+                        inline
+                        label="Satın alınmış dersler"
+                        id={`inline-${type}-1`}
+                      />
+                      <br />
+                      <Form.Check
+                        inline
+                        label="Başlanmamış dersler"
+                        id={`inline-${type}-1`}
+                      />
+                    </div>
+                  ))}
+                </Form>
               </div>
             </div>
           </div>
