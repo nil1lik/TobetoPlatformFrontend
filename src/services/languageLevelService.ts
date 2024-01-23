093e31a28450a57abf416b7d094f95ba780ced1b
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-23 12:18:38 +0300

    page sayfası değiştirildi

--- src/services/languageLevelService.ts
+++ src/services/languageLevelService.ts
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export default class LanguageLevelService{
   getLanguageLevel(){
-    return axios.get("http://localhost:5278/api/LanguageLevels?PageIndex=0&PageSize=4")
+    return axios.get("http://localhost:5278/api/LanguageLevels?PageIndex=0&PageSize=5")
   }
 }
