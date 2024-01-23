e5e024c8e2d057a8cd8f73d8a6948df7f40eecc4
Nil <144815514+nil1lik@users.noreply.github.com>
2024-01-23 18:35:54 +0300

    skill service bugs fixed

--- src/services/skillService.ts
+++ src/services/skillService.ts
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export default class SkillService{
   getSkill(){
-    return axios.get("http://localhost:5278/api/Skills")
+    return axios.get("http://localhost:5278/api/Skills?PageIndex=0&PageSize=25")
   }
 }
