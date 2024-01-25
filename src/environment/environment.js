4d88ca1275ec2d93f94b9aa8adfe549655128ab2
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-01-25 06:31:10 +0300

    Skill base yapıya taşındı. aynı sayfada  Submit butonu etkinleştirildi.

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BASE_API_URL = void 0;
-exports.BASE_API_URL = "https://localhost:8080/api/";
+exports.BASE_API_URL = "http://localhost:5278/api/";
