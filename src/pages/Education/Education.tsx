6fed593f504618aad1618ed0f2624413e7f4a404
Nida <nidakul@hotmail.com>
2024-01-25 15:31:44 +0300

    Education sayfası düzenlendi

--- src/pages/Education/Education.tsx
+++ src/pages/Education/Education.tsx
@@ -2,18 +2,56 @@ import React from "react";
 import TobetoPlatformBannerTop from "../../utilities/tobetoPlatform/TobetoPlatformBannerTop";
 import TobetoPlatformDropdown from "../../utilities/tobetoPlatform/TobetoPlatformDropdown";
 import TobetoPlatformSearchBar from "../../utilities/tobetoPlatform/TobetoPlatformSearchBar";
+import "./education.css";
+import { Col, Container, Row } from "react-bootstrap";
+import TobetoPlatformTab from "../../utilities/tobetoPlatform/TobetoPlatformTab";
+import EducationCard from "../../components/EducationCard/EducationCard";
 
 type Props = {};
 
 const Education = (props: Props) => {
   return (
     <>
-    <TobetoPlatformBannerTop url="https://tobeto.com/_next/static/media/edu-banner3.d7dc50ac.svg" spanText="Eğitimlerim"/>
-    <TobetoPlatformSearchBar/>
-    <TobetoPlatformDropdown splitBtn="Tür" act1="deneme 1" act2="deneme2" act3="deneme3" />
-    <TobetoPlatformDropdown splitBtn="Organizasyon" act1="deneme 1" act2="deneme2" act3="deneme3"/>
-    <TobetoPlatformDropdown splitBtn="Sıralama" act1="deneme 1" act2="deneme2" act3="deneme3"/>
-      </>
+      <TobetoPlatformBannerTop
+        url="https://tobeto.com/_next/static/media/edu-banner3.d7dc50ac.svg"
+        spanText="Eğitimlerim"
+      />
+      <Container>
+        <div className="filter-section mt-3">
+          <Row>
+            <Col className="col-md-5 col-12 mb-4">
+              <TobetoPlatformSearchBar />
+            </Col>
+            <Col>
+              <TobetoPlatformDropdown
+                act1="İstanbul Kodluyor"
+                showDefaultOption={true}
+              />
+            </Col>
+            <Col>
+              <TobetoPlatformDropdown
+                act1="Adına Göre (A-Z)"
+                act2="Adına Göre (Z-A)"
+                act3="Tarihe Göre (Y-E)"
+                act4="Tarihe Göre (E-Y)"
+              />
+            </Col>
+            <Col></Col>
+          </Row>
+        </div>
+
+        <Row className="mt-3 row">
+          <div className="col-12 mb-4">
+            <div className="nav nav-tabs mainTablist d-flex justify-content-center">
+              <TobetoPlatformTab />
+            </div>
+          </div>
+          <div className="col-12">
+            <EducationCard></EducationCard>
+          </div>
+        </Row>
+      </Container>
+    </>
   );
 };
 export default Education;
--- src/utilities/tobetoPlatform/TobetoPlatformSearchBar.tsx
+++ src/utilities/tobetoPlatform/TobetoPlatformSearchBar.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import { Button, Col, Form, Image, Row } from "react-bootstrap";
-
-type Props = {}
+import { Button, Col, Form, Image, InputGroup, Row } from "react-bootstrap";
+type Props = {};
 
 const TobetoPlatformSearchBar = (props: Props) => {
   return (
     <div>
-      <Form>
-        <Row>
-          <Col xs="auto">
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className=" mr-sm-2"
-            />
-          </Col>
-        </Row>
-      </Form>
+      <InputGroup>
+        <Form.Control type="text" placeholder="Arama" className="mr-sm-2" />
+        <Button className="search-btn">
+            <img src="../images/search.svg" />
+        </Button>
+      </InputGroup> 
     </div>
-  )
-}
+  );
+};
 
-export default TobetoPlatformSearchBar
+export default TobetoPlatformSearchBar;
