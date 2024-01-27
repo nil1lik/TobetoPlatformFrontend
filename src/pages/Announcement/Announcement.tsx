917a417c4dcb4aa7946cabd90622642b912861d3
Nil <144815514+nil1lik@users.noreply.github.com>
2024-01-27 15:40:51 +0300

    announcement (show more) page created

--- src/pages/Announcement/Announcement.tsx
+++ src/pages/Announcement/Announcement.tsx
@@ -1,12 +1,87 @@
-import React from 'react'
-import TobetoPlatformBannerTop from '../../utilities/tobetoPlatform/TobetoPlatformBannerTop'
+import React, { useEffect, useState } from "react";
+import TobetoPlatformBannerTop from "../../utilities/tobetoPlatform/TobetoPlatformBannerTop";
+import { Container, Pagination } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import TobetoPlatformSearchBar from "../../utilities/tobetoPlatform/TobetoPlatformSearchBar";
+import TobetoPlatformDropdown from "../../utilities/tobetoPlatform/TobetoPlatformDropdown";
+import AnnouncementCard from "../../components/Announcement/AnnouncementCard";
+import announcementService from "../../services/announcementService";
+import { GetAnnouncementTypeItem } from "../../models/responses/announcement/getAnnouncementTypeList";
+import "./announcement.css";
 
-type Props = {}
+type Props = {};
 
 const Announcement = (props: Props) => {
-  return (
-    <TobetoPlatformBannerTop url="https://tobeto.com/_next/static/media/edu-banner3.d7dc50ac.svg" spanText="Duyurularım"/>
-  )
-}
+  const announcementIconSrc =
+    process.env.PUBLIC_URL + `/images/announcementDate.svg`;
+  const [announcement, setAnnouncement] = useState<GetAnnouncementTypeItem[]>(
+    []
+  );
 
-export default Announcement
+  useEffect(() => {
+    const fetchAnnouncement = async () => {
+      const result = await announcementService.getAllAnnouncementTypeList(0, 9);
+      setAnnouncement(result.data.items);
+    };
+    fetchAnnouncement();
+  }, []);
+
+  return (
+    <>
+      <TobetoPlatformBannerTop
+        url="https://tobeto.com/_next/static/media/edu-banner3.d7dc50ac.svg"
+        spanText="Duyurularım"
+      />
+      <Container>
+        <div className="filter-section mt-3">
+          <Row>
+            <Col className="col-md-5 col-12 mb-4">
+              <TobetoPlatformSearchBar />
+            </Col>
+            <Col>
+              <TobetoPlatformDropdown
+                dropdownName="Organizasyon"
+                act1="İstanbul Kodluyor"
+                showDefaultOption={true}
+              />
+            </Col>
+            <Col>
+              <TobetoPlatformDropdown
+                dropdownName="Sıralama"
+                act1="Adına Göre (A-Z)"
+                act2="Adına Göre (Z-A)"
+                act3="Tarihe Göre (Y-E)"
+                act4="Tarihe Göre (E-Y)"
+                showDefaultOption={true}
+              />
+            </Col>
+            <Col></Col>
+          </Row>
+        </div>
+        <Row className="announcement-card-line">
+          {announcement.map((announcement: any) => (
+            <AnnouncementCard
+              announcementType={announcement.announcementTypeName}
+              announcementName={announcement.name}
+              announcementTitle={announcement.title}
+              annoucementDateIcon={announcementIconSrc}
+              announcementDate={announcement.createdDate}
+              announcementDescription={announcement.description}
+            ></AnnouncementCard>
+          ))}
+        </Row>
+        <Row className="pagination">
+        <Pagination>
+          <Pagination.Prev />
+          <Pagination.Item>{1}</Pagination.Item>
+          <Pagination.Item>{2}</Pagination.Item>
+          <Pagination.Next />
+        </Pagination>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Announcement;
