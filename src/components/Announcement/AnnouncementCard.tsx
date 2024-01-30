1f2464de812d47dce935ca37aeb295f5a69d8dbd
Eren Macit <ernmctt@gmail.com>
2024-01-30 21:17:36 +0300

    Feat: Exam => ExamBox, ExamCard & Popup Component Added

--- src/components/Announcement/AnnouncementCard.tsx
+++ src/components/Announcement/AnnouncementCard.tsx
@@ -18,61 +18,39 @@ const AnnouncementCard = (props: Props) => {
   const handleShow = () => setShow(true);
 
   return (
-    <div className="col-md-4 col-12">
-      <Card className="card-notify">
-        <div className="announcementTopCont">
-          <Card.Body className="announcementInfoTop">
-            <span>{props.announcementType}</span>
-            <span>{props.announcementName}</span>
+    <>
+      <div className="col-md-4 col-12">
+        <Card className="card-notify">
+          <div className="announcementTopCont">
+            <Card.Body className="announcementInfoTop">
+              <span>{props.announcementType}</span>
+              <span>{props.announcementName}</span>
+            </Card.Body>
+            <Card.Body className="announcementHeader">
+              <span>{props.announcementTitle}</span>
+            </Card.Body>
+          </div>
+          <Card.Body className="announcementInfoBottom">
+            <span>
+              <Image
+                src={props.annoucementDateIcon?.toString()}
+                className="dateIcon"
+              />
+              {new Date(props.announcementDate).toLocaleDateString("tr-TR")}
+            </span>
+            <span className="announcementReadMore" onClick={handleShow}>
+              Devamını Oku
+            </span>
           </Card.Body>
-          <Card.Body className="announcementHeader">
-            <span>{props.announcementTitle}</span>
-          </Card.Body>
-        </div>
-        <Card.Body className="announcementInfoBottom">
-          <span>
-            <Image
-              src={props.annoucementDateIcon?.toString()}
-              className="dateIcon"
-            />
-            {new Date(props.announcementDate).toLocaleDateString("tr-TR")}
-          </span>
-          <span className="announcementReadMore" onClick={handleShow}>
-            Devamını Oku
-          </span>
-        </Card.Body>
-      </Card>
-
-      <Popup title={props.announcementTitle} description={props.announcementDescription} show={show} hide={handleClose}/>
-
-
-
-
-
-
-
-
-
-      {/* <Modal size="lg" show={show} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title style={{ fontWeight: "600" }}>
-            {props.announcementTitle}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {`${props.announcementDescription}`.split("\n").map((line, index) => (
-            <>
-              <p key={index} className="modal-text">
-                {line}
-              </p>
-              <p>
-                <br />
-              </p>
-            </>
-          ))}
-        </Modal.Body>
-      </Modal> */}
-    </div>
+        </Card>
+      </div>
+      <Popup
+        title={props.announcementTitle}
+        description={props.announcementDescription}
+        show={show}
+        hide={handleClose}
+      />
+    </>
   );
 };
 
---
+++ src/components/Exam/ExamCard.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
+import Popup from "../Popup/Popup";
+
+type Props = {
+  id: number;
+  name: string;
+  description: string;
+  duration: string;
+};
+
+const ExamCard = (props: Props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Card className="card-transition" onClick={handleShow}>
+        <Card.Body key={props.id} className="exam-card card-title h5">
+          <Card.Title>{props.name}</Card.Title>
+          <Card.Img
+            className="status-done"
+            src={process.env.PUBLIC_URL + "/images/status-done.svg"}
+          />
+          <Card.Subtitle className="mb-2 text-muted">
+            {props.description}
+          </Card.Subtitle>
+          <div className="exam-icon-text">
+            <Card.Img
+              className="exam-time"
+              src={process.env.PUBLIC_URL + "/images/examTime.svg"}
+            />
+            <Card.Text className="exam-time-text">{props.duration}</Card.Text>
+          </div>
+        </Card.Body>
+      </Card>
+      <Popup
+        key={props.id}
+        title={props.name}
+        description={props.description}
+        duration={props.duration}
+        show={show}
+        hide={handleClose}
+      />
+    </>
+  );
+};
+
+export default ExamCard;
