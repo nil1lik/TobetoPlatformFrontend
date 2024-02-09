c5e0652032d146964905f3ace907b6a953497649
nil1lik <birliknil@gmail.com>
2024-02-09 14:51:28 +0300

    feat: magic strings refactoring

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { pageIsNotFound } from "../../utilities/Constants/constantValues";
 
 type Props = {};
 
 const NotFound = (props: Props) => {
-	return <div>NotFound</div>;
+	return <div>{pageIsNotFound}</div>;
 };
 
 export default NotFound;
