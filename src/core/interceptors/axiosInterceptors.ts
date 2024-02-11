84b76c788909780af2ec62c104b4bfb464dafd53
hfzehra <hfzehra.uysal33@gmail.com>
2024-02-11 22:11:48 +0300

    deleted token

--- src/core/interceptors/axiosInterceptors.ts
+++ src/core/interceptors/axiosInterceptors.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { BASE_API_URL } from "../environment/environment";
-import tokenService from "../../services/tokenService";
+import tokenService from "../services/tokenService";
 
 const axiosInstance = axios.create({
     baseURL: BASE_API_URL 
