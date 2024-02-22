1c953297bbea78b7e48becf1804fb300d6759f49
Eren Macit <ernmctt@gmail.com>
2024-02-22 14:10:03 +0300

    feat: Profile Page => Success Model Default

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,37 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap'
-import ProfilePreInfoBox from '../../components/Profile/ProfileLeft/ProfilePreInfoBox/ProfilePreInfoBox'
-import './profile.css'
-import ProfileBox from '../../components/Profile/ProfileBox'
-import ProfilePreInfo from '../../components/Profile/ProfileLeft/ProfilePreInfoBox/ProfilePreInfo'
-import ProfileRoundItem from '../../components/Profile/ProfileLeft/ProfileRoundItem'
-import ProfileMediaAccounts from '../../components/Profile/ProfileLeft/ProfileMediaAccounts'
-import ProfileSuccessModel from '../../components/Profile/ProfileRight/ProfileSuccessModel/ProfileSuccessModel'
-import { Link } from 'react-router-dom'
-import ProfileBadge from '../../components/Profile/ProfileRight/ProfileBadge'
-import ProfileExam from '../../components/Profile/ProfileRight/ProfileExam'
-import ProfileEducationMap from '../../components/Profile/ProfileRight/ProfileEducationMap'
-import ProfileHeatMap from '../../components/Profile/ProfileRight/ProfileHeatMap'
-import { useAuthContext } from '../../contexts/AuthContext'
-import userProfileService from '../../services/userProfileService'
-import { GetByIdUser } from '../../models/responses/user/getByIdUser'
+import React, { useEffect, useState } from "react";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
+import ProfilePreInfoBox from "../../components/Profile/ProfileLeft/ProfilePreInfoBox/ProfilePreInfoBox";
+import "./profile.css";
+import ProfileBox from "../../components/Profile/ProfileBox";
+import ProfilePreInfo from "../../components/Profile/ProfileLeft/ProfilePreInfoBox/ProfilePreInfo";
+import ProfileRoundItem from "../../components/Profile/ProfileLeft/ProfileRoundItem";
+import ProfileMediaAccounts from "../../components/Profile/ProfileLeft/ProfileMediaAccounts";
+import ProfileSuccessModel from "../../components/Profile/ProfileRight/ProfileSuccessModel/ProfileSuccessModel";
+import { Link } from "react-router-dom";
+import ProfileBadge from "../../components/Profile/ProfileRight/ProfileBadge";
+import ProfileExam from "../../components/Profile/ProfileRight/ProfileExam";
+import ProfileEducationMap from "../../components/Profile/ProfileRight/ProfileEducationMap";
+import ProfileHeatMap from "../../components/Profile/ProfileRight/ProfileHeatMap";
+import { useAuthContext } from "../../contexts/AuthContext";
+import userProfileService from "../../services/userProfileService";
+import { GetByIdUser } from "../../models/responses/user/getByIdUser";
 
-type Props = {
-}
+type Props = {};
 
 const Profile = (props: Props) => {
+  const [user, setUser] = useState<GetByIdUser>();
+  const { userId } = useAuthContext();
+  const [successModel, setSuccessModel] = useState<boolean>(false);
 
-  const [user, setUser] = useState<GetByIdUser>()
-  const { userId} = useAuthContext();
-
-  const fethUserData = async (userId:number) => {
-    const result = await userProfileService.getByUserId(userId)
-    setUser(result.data)
-  }
+  const fethUserData = async (userId: number) => {
+    const result = await userProfileService.getByUserId(userId);
+    setUser(result.data);
+  };
 
   useEffect(() => {
-    fethUserData(Number(userId))
-  }, [userId])
+    fethUserData(Number(userId));
+  }, [userId]);
 
   // const png = process.env.PUBLIC_URL + `/images/png.png`;
 
@@ -40,28 +39,30 @@ const Profile = (props: Props) => {
       <Row>
         <Col xs={12} style={{ textAlign: "right" }}>
           <Link to={"/profilim/profilimi-duzenle"}>
-            <span className='profileEditButton'></span>
+            <span className="profileEditButton"></span>
           </Link>
-          <span className='profileShareButton'></span>
+          <span className="profileShareButton"></span>
         </Col>
       </Row>
       <Row>
         {/* PROFILE LEFT START */}
-        <Col className='col-4'>
+        <Col className="col-4">
           <Row>
-            <Col className='col-12'>
-              <ProfilePreInfoBox profilePhotoSrc='pp.png' />
+            <Col className="col-12">
+              <ProfilePreInfoBox profilePhotoSrc="pp.png" />
             </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Hakkımda'>
+            <Col className="col-12">
+              <ProfileBox titleClass="profileBoxTitle" title="Hakkımda">
                 <Card.Text>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore eius corrupti tempora unde ea facilis! Ratione neque quibusdam fugiat doloremque.
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Labore eius corrupti tempora unde ea facilis! Ratione neque
+                  quibusdam fugiat doloremque.
                 </Card.Text>
               </ProfileBox>
             </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Yetkinliklerim'>
-                <div className='profileRoundItemCont'>
+            <Col className="col-12">
+              <ProfileBox titleClass="profileBoxTitle" title="Yetkinliklerim">
+                <div className="profileRoundItemCont">
                   <ProfileRoundItem className="profileRoundItem">
                     {<Card.Text>HTML</Card.Text>}
                   </ProfileRoundItem>
@@ -74,31 +75,59 @@ const Profile = (props: Props) => {
                 </div>
               </ProfileBox>
             </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Yabancı Diller'>
-                <div className='profileRoundItemCont'>
-                  <ProfilePreInfo cardContClass='profileLangCont' iconContClass='' headerClass='profileSkillName' valueClass='profileSkillLevel' iconSrc='globe.svg' header='İngilizce' value='Orta Seviye' />
+            <Col className="col-12">
+              <ProfileBox titleClass="profileBoxTitle" title="Yabancı Diller">
+                <div className="profileRoundItemCont">
+                  <ProfilePreInfo
+                    cardContClass="profileLangCont"
+                    iconContClass=""
+                    headerClass="profileSkillName"
+                    valueClass="profileSkillLevel"
+                    iconSrc="globe.svg"
+                    header="İngilizce"
+                    value="Orta Seviye"
+                  />
                 </div>
               </ProfileBox>
             </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Sertifikalarım'>
-                <div className='profileRoundItemCont'>
+            <Col className="col-12">
+              <ProfileBox titleClass="profileBoxTitle" title="Sertifikalarım">
+                <div className="profileRoundItemCont">
                   <ProfileRoundItem className="profileRoundItem hover">
-                    {<Card.Text className='profileCertificate'>Lorem, ipsum dolor.</Card.Text>}
+                    {
+                      <Card.Text className="profileCertificate">
+                        Lorem, ipsum dolor.
+                      </Card.Text>
+                    }
                   </ProfileRoundItem>
                   <ProfileRoundItem className="profileRoundItem hover">
-                    {<Card.Text className='profileCertificate'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Card.Text>}
+                    {
+                      <Card.Text className="profileCertificate">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      </Card.Text>
+                    }
                   </ProfileRoundItem>
                 </div>
               </ProfileBox>
             </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Medya Hesaplarım'>
-                <div className='profileMediaCont'>
-                  <ProfileMediaAccounts imageSrc="cv-github.svg" className='mediaAccountPhoto' Link='https://www.github.com'/>
-                  <ProfileMediaAccounts imageSrc="cv-linkedn.svg" className='mediaAccountPhoto' Link='https://www.linkedin.com'/>
-                  <ProfileMediaAccounts imageSrc="cv-behance.svg" className='mediaAccountPhoto' Link='https://www.behance.net'/>
+            <Col className="col-12">
+              <ProfileBox titleClass="profileBoxTitle" title="Medya Hesaplarım">
+                <div className="profileMediaCont">
+                  <ProfileMediaAccounts
+                    imageSrc="cv-github.svg"
+                    className="mediaAccountPhoto"
+                    Link="https://www.github.com"
+                  />
+                  <ProfileMediaAccounts
+                    imageSrc="cv-linkedn.svg"
+                    className="mediaAccountPhoto"
+                    Link="https://www.linkedin.com"
+                  />
+                  <ProfileMediaAccounts
+                    imageSrc="cv-behance.svg"
+                    className="mediaAccountPhoto"
+                    Link="https://www.behance.net"
+                  />
                 </div>
               </ProfileBox>
             </Col>
@@ -106,41 +135,78 @@ const Profile = (props: Props) => {
         </Col>
         {/* PROFILE LEFT END */}
         {/* PROFILE RIGHT START */}
-        <Col className='col-8'>
+        <Col className="col-8">
           <Row>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Tobeto İşte Başarı Modelim'>
-                <ProfileSuccessModel />
+            <Col className="col-12">
+              <ProfileBox
+                titleClass="profileBoxTitle"
+                title="Tobeto İşte Başarı Modelim"
+              >
+                {successModel ? (
+                  <ProfileSuccessModel />
+                ) : (
+                  <div className="successModelDefault">
+                    <p>
+                      İşte Başarı Modeli Değerlendirmesiyle yetkinliklerini ölç
+                    </p>
+                    <button>Başla</button>
+                  </div>
+                )}
               </ProfileBox>
             </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Tobeto Seviye Testlerim'>
-              <div className='profileExamsCont'>
-                <ProfileExam profileExamName='Herkes için Kodlama 1B Değerlendirme Sınavı' profileExamDate='12-10-2023' profileExamPoint='88.00'/>
-                <ProfileExam profileExamName='Front End' profileExamDate='12-10-2023' profileExamPoint='88.00'/>
-                <ProfileExam profileExamName='Herkes için Kodlama 1B Değerlendirme Sınavı' profileExamDate='17-11-2023' profileExamPoint='88.00'/>
-                <ProfileExam profileExamName='Back End' profileExamDate='17-11-2023' profileExamPoint='88.00'/>
-              </div>
-              </ProfileBox>
-            </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Yetkinlik Rozetlerim'>
-                <div className='profileBadgeMainCont'>
-                  <ProfileBadge imageSrc='istanbulkodluyorbadge.jpg' />
-                  <ProfileBadge imageSrc='isbecerileribadge.jpg' />
-                  <ProfileBadge imageSrc='isyönetimibecerileribadge.jpg' />
-                  <ProfileBadge imageSrc='isyönetimibecerileribadge2.jpg' />
-                  <ProfileBadge imageSrc='kisiselgelisimbadge.jpg' />
+            <Col className="col-12">
+              <ProfileBox
+                titleClass="profileBoxTitle"
+                title="Tobeto Seviye Testlerim"
+              >
+                <div className="profileExamsCont">
+                  <ProfileExam
+                    profileExamName="Herkes için Kodlama 1B Değerlendirme Sınavı"
+                    profileExamDate="12-10-2023"
+                    profileExamPoint="88.00"
+                  />
+                  <ProfileExam
+                    profileExamName="Front End"
+                    profileExamDate="12-10-2023"
+                    profileExamPoint="88.00"
+                  />
+                  <ProfileExam
+                    profileExamName="Herkes için Kodlama 1B Değerlendirme Sınavı"
+                    profileExamDate="17-11-2023"
+                    profileExamPoint="88.00"
+                  />
+                  <ProfileExam
+                    profileExamName="Back End"
+                    profileExamDate="17-11-2023"
+                    profileExamPoint="88.00"
+                  />
                 </div>
               </ProfileBox>
             </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Aktivite Haritam'>
+            <Col className="col-12">
+              <ProfileBox
+                titleClass="profileBoxTitle"
+                title="Yetkinlik Rozetlerim"
+              >
+                <div className="profileBadgeMainCont">
+                  <ProfileBadge imageSrc="istanbulkodluyorbadge.jpg" />
+                  <ProfileBadge imageSrc="isbecerileribadge.jpg" />
+                  <ProfileBadge imageSrc="isyönetimibecerileribadge.jpg" />
+                  <ProfileBadge imageSrc="isyönetimibecerileribadge2.jpg" />
+                  <ProfileBadge imageSrc="kisiselgelisimbadge.jpg" />
+                </div>
+              </ProfileBox>
+            </Col>
+            <Col className="col-12">
+              <ProfileBox titleClass="profileBoxTitle" title="Aktivite Haritam">
                 <ProfileHeatMap />
               </ProfileBox>
             </Col>
-            <Col className='col-12'>
-              <ProfileBox titleClass='profileBoxTitle' title='Eğitim Hayatım ve Deneyimlerim'>
+            <Col className="col-12">
+              <ProfileBox
+                titleClass="profileBoxTitle"
+                title="Eğitim Hayatım ve Deneyimlerim"
+              >
                 <ProfileEducationMap />
               </ProfileBox>
             </Col>
@@ -152,7 +218,7 @@ const Profile = (props: Props) => {
     // <Link to={'/profilim/profilimi-duzenle'}>
     //   <div>Profile</div>
     // </Link>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
