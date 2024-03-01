f495ec9fc4a2cf470d799234c53a04bffb7c5930
Eren Macit <ernmctt@gmail.com>
2024-03-01 23:19:11 +0300

    update: Login Page Style

---
+++ src/pages/Login/FormBg.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+
+type Props = {};
+
+const FormBg = (props: Props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 1500">
+      <rect fill="#000000" width="2000" height="1500" />
+      <defs>
+        <rect
+          fill="none"
+          strokeWidth="41.3"
+          strokeOpacity="1"
+          id="a"
+          x="-400"
+          y="-300"
+          width="800"
+          height="600"
+        />
+      </defs>
+      <g transform="" style={{ transformOrigin: "center" }}>
+        <g transform="rotate(162 0 0)" style={{ transformOrigin: "center" }}>
+          <g transform="rotate(-160 0 0)" style={{ transformOrigin: "center" }}>
+            <g transform="translate(1000 750)">
+              <use
+                stroke="#0D000E"
+                href="#a"
+                transform="rotate(10 0 0) scale(1.1)"
+              />
+              <use
+                stroke="#11021d"
+                href="#a"
+                transform="rotate(20 0 0) scale(1.2)"
+              />
+              <use
+                stroke="#16032d"
+                href="#a"
+                transform="rotate(30 0 0) scale(1.3)"
+              />
+              <use
+                stroke="#1a053c"
+                href="#a"
+                transform="rotate(40 0 0) scale(1.4)"
+              />
+              <use
+                stroke="#1f074b"
+                href="#a"
+                transform="rotate(50 0 0) scale(1.5)"
+              />
+              <use
+                stroke="#23095a"
+                href="#a"
+                transform="rotate(60 0 0) scale(1.6)"
+              />
+              <use
+                stroke="#280a6a"
+                href="#a"
+                transform="rotate(70 0 0) scale(1.7)"
+              />
+              <use
+                stroke="#2c0c79"
+                href="#a"
+                transform="rotate(80 0 0) scale(1.8)"
+              />
+              <use
+                stroke="#310e88"
+                href="#a"
+                transform="rotate(90 0 0) scale(1.9)"
+              />
+              <use
+                stroke="#351097"
+                href="#a"
+                transform="rotate(100 0 0) scale(2)"
+              />
+              <use
+                stroke="#3a11a7"
+                href="#a"
+                transform="rotate(110 0 0) scale(2.1)"
+              />
+              <use
+                stroke="#3e13b6"
+                href="#a"
+                transform="rotate(120 0 0) scale(2.2)"
+              />
+              <use
+                stroke="#4315c5"
+                href="#a"
+                transform="rotate(130 0 0) scale(2.3)"
+              />
+              <use
+                stroke="#4717d4"
+                href="#a"
+                transform="rotate(140 0 0) scale(2.4)"
+              />
+              <use
+                stroke="#4c18e4"
+                href="#a"
+                transform="rotate(150 0 0) scale(2.5)"
+              />
+              <use
+                stroke="#501AF3"
+                href="#a"
+                transform="rotate(160 0 0) scale(2.6)"
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default FormBg;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import LoginPageBox from "../../components/Login/LoginPageBox";
 import { Container, Image } from "react-bootstrap";
 import "./login.css";
