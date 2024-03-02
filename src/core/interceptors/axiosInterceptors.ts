be034f431543526a3a31e4e0df9b793e22b7addc
nil1lik <birliknil@gmail.com>
2024-03-02 19:00:16 +0300

    fix: jwt solve moved to backend

--- src/core/interceptors/axiosInterceptors.ts
+++ src/core/interceptors/axiosInterceptors.ts
@@ -1,7 +1,6 @@
 import toastr from "toastr";
 import axios from 'axios';
 import { BASE_API_URL } from '../environment/environment';
-import { parseJwt } from '../../utilities/Constants/parseJwt';
 
 const axiosInstance = axios.create({
     baseURL: BASE_API_URL,
@@ -10,9 +9,9 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
     request => {
         const token = localStorage.getItem("token");
-        if (token) {
-            request.headers.Authorization = `Bearer ${token}`;
-        }
+        // if (token) {
+        //     request.headers.Authorization = `Bearer ${token}`;
+        // }
         return request;
     },
     error => {
@@ -22,20 +21,15 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
     response => {
-
         const accessToken = response.data.accessToken;
+        const userId = response.data.userId;
         if (accessToken) {
             const token = accessToken.token;
+            response.headers["userId"] = userId;
             localStorage.setItem("token", token);
-            const decodedToken = parseJwt(token);
-            const userId = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
-            console.log(userId);
-            response.headers['userId'] = userId;
-            console.log("Kullanıcı ID:", userId);
         }
         toastr.success("Giriş Başarılı");
         return response;
-
     },
     error => {
         console.error(error);
@@ -49,4 +43,3 @@ axiosInstance.interceptors.response.use(
 )
 
 export default axiosInstance;
-
