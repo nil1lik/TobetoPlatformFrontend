17e9ef4e55cb1f715b3fc8e8d8d8fab11aeebbfb
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-03-05 12:45:44 +0300

    axiosIntercepptor edited

--- src/core/interceptors/axiosInterceptors.ts
+++ src/core/interceptors/axiosInterceptors.ts
@@ -10,9 +10,9 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
     request => {
         const token = localStorage.getItem("token");
-        // if (token) {
-        //     request.headers.Authorization = `Bearer ${token}`;
-        // }
+        if (token) {
+            request.headers.Authorization = `Bearer ${token}`;
+        }
         return request;
     },
     error => {
